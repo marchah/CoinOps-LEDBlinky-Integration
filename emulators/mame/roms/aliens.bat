22e3ab1360cdfa3bbdc0944444da2949dcaf6d7f
marchah <marchah.dev@gmail.com>
2020-08-27 18:21:10 -0700

    add change necessary to make LEdBlinky works

---
+++ emulators/mame/roms/aliens.bat
@@ -0,0 +1,28 @@
+@echo off
+cd..
+cd..
+cd LEDBlinky
+start "" LEDBlinky.exe aliens
+cd..
+cd mame
+
+rem Use the following mame version depending on the following:
+
+rem Use 'mame64.exe' (v0.196 roms) or 'mameNEW64.exe' (later v0.215 roms) if using a 64-bit CPU / OS, otherwise use "mame.exe" for 32-bit CPU / OS.
+
+start /wait "" mameNEW64.exe aliens
+ 
+:WAITLOOP
+tasklist /FI "IMAGENAME eq mameNEW64.exe" 2>NUL | find /I /N "mameNEW64.exe">NUL
+if "%ERRORLEVEL%"=="0" goto RUNNING
+goto NOTRUNNING
+
+:RUNNING
+timeout /t 5
+goto WAITLOOP
+
+:NOTRUNNING
+taskkill /f /im "LEDBlinky.exe"
+cd..
+cd LEDBlinky
+start "" LEDBlinky.exe random.lwax
