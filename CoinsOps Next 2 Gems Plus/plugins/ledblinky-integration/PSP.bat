56ea85b5c9e450a06538c99985567e909ef56edf
marchah <marchah.dev@gmail.com>
2020-08-30 15:01:32 -0700

    add bat for most systems
    most of them don't work yet ...

---
+++ CoinsOps Next 2 Gems Plus/plugins/ledblinky-integration/PSP.bat
@@ -0,0 +1,23 @@
+@echo off
+call config.cmd
+set rom_path=%1
+set rom_name=%2
+
+cd..\LEDBlinky
+start "" LEDBlinky.exe %rom_name% RETROARCH
+
+cd..\..\emulators\RetroArchXiso
+start /wait "" retroarch.exe -L cores\ppsspp_libretro.dll %rom_path%
+
+:WAITLOOP
+tasklist /FI "IMAGENAME eq retroarch.exe" 2>NUL | find /I /N "retroarch.exe">NUL
+if "%ERRORLEVEL%"=="0" goto RUNNING
+goto NOTRUNNING
+
+:RUNNING
+timeout /t 5
+goto WAITLOOP
+
+:NOTRUNNING
+cd..\..\plugins\LEDBlinky
+start "" LEDBlinky.exe %frontend_default_animation%
---
+++ CoinsOps Next 2 Gems Plus/plugins/ledblinky-integration/SNES.bat
@@ -0,0 +1,23 @@
+@echo off
+call config.cmd
+set rom_path=%1
+set rom_name=%2
+
+cd..\LEDBlinky
+start "" LEDBlinky.exe %rom_name% MAME
+
+cd..\..\emulators\mame
+start /wait "" mame.exe snes -cart "%rom_path%" -view %rom_name%
+
+:WAITLOOP
+tasklist /FI "IMAGENAME eq mame.exe" 2>NUL | find /I /N "mame.exe">NUL
+if "%ERRORLEVEL%"=="0" goto RUNNING
+goto NOTRUNNING
+
+:RUNNING
+timeout /t 5
+goto WAITLOOP
+
+:NOTRUNNING
+cd..\..\plugins\LEDBlinky
+start "" LEDBlinky.exe %frontend_default_animation%
