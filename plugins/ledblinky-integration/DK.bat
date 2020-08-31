5884de77291ccb650f540fdcf1f93cbec0c7dc0e
marchah <marchah.dev@gmail.com>
2020-08-30 21:16:54 -0700

    feat: add missing conf

---
+++ plugins/ledblinky-integration/DK.bat
@@ -0,0 +1,24 @@
+@echo off
+call config.cmd
+set rom_path=%1
+set rom_name=%2
+
+
+cd..\LEDBlinky
+start "" LEDBlinky.exe gnw_dkong MAME
+
+cd..\..\emulators\mame
+start /wait "" mameNEW64.exe gnw_dkong
+
+:WAITLOOP
+tasklist /FI "IMAGENAME eq mame64.exe" 2>NUL | find /I /N "mame64.exe">NUL
+if "%ERRORLEVEL%"=="0" goto RUNNING
+goto NOTRUNNING
+
+:RUNNING
+timeout /t 5
+goto WAITLOOP
+
+:NOTRUNNING
+cd..\..\plugins\LEDBlinky
+start "" LEDBlinky.exe %frontend_default_animation%
