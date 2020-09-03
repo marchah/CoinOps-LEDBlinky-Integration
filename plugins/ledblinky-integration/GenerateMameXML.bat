73070eb4917e9370917dc9d121690cfb9ac55126
marchah <marchah.dev@gmail.com>
2020-09-02 19:58:28 -0700

    fix: GenerateMameXML.bat file name

--- plugins/ledblinky-integration/GenerateMameXML.bat
+++ plugins/ledblinky-integration/GenerateMameXML.bat
@@ -1,5 +1,5 @@
 @echo off
 
 cd ..\..\emulators\MAME
-mame64.exe -listxml 1>mameTest.xml
-move mameTest.xml ..\..\plugins\ledblinky-integration\
+mame64.exe -listxml 1>mame.xml
+move mame.xml ..\..\plugins\ledblinky-integration\
