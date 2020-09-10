d9aad75961e1ae05943eee26a24d6404703cd04e
marchah <marchah.dev@gmail.com>
2020-09-09 21:04:36 -0700

    feat: update consoles config

--- scripts/checkMissingRomsConfig.ts
+++ scripts/checkMissingRomsConfig.ts
@@ -1,11 +1,12 @@
-import { promises, access } from 'fs';
+import { promises } from 'fs';
 import { startsWith } from 'lodash';
 
 const colorConfPath = './plugins/ledblinky-integration/color-config/Colors.ini';
 const controlConfPath =
   './plugins/ledblinky-integration/color-config/Controls.ini';
 
-const gemsPlusRomsList = './scripts/listMAMERomsGemPlus.txt';
+const gemsRomsList = './scripts/listMAMERomsGems.txt';
+const gemsPlusRomsList = './scripts/listMAMERomsGemsPlus.txt';
 
 const COINOPS_MAME_ROMS: { [type: string]: string[] } = {};
 
@@ -21,24 +22,14 @@ function loadIniFileRomNames(configFile: string): string[] {
   return romNames;
 }
 
-(async () => {
-  const controlsFile = await promises.readFile(controlConfPath, {
-    encoding: 'utf-8',
-  });
-  const colorsFile = await promises.readFile(colorConfPath, {
-    encoding: 'utf-8',
-  });
-
-  const controlsRomNames = loadIniFileRomNames(controlsFile);
-  const colorsRomNames = loadIniFileRomNames(colorsFile);
-
-  COINOPS_MAME_ROMS.GEMS_PLUS = (
-    await promises.readFile(gemsPlusRomsList, {
-      encoding: 'utf-8',
-    })
-  ).split('\n');
-
-  const gemsPlusInfos = COINOPS_MAME_ROMS.GEMS_PLUS.reduce(
+function checkRomsConfig(
+  roms: string[],
+  controlsRomNames: string[],
+  colorsRomNames: string[]
+): {
+  [romName: string]: { controls: boolean; colors: boolean };
+} {
+  return roms.reduce(
     (acc, rom) => {
       const hasControls = !!controlsRomNames.find((str) => str === rom);
       const hasColors = !!colorsRomNames.find((str) => str === rom);
@@ -55,10 +46,51 @@ function loadIniFileRomNames(configFile: string): string[] {
       [romName: string]: { controls: boolean; colors: boolean };
     }
   );
+}
+
+(async () => {
+  const controlsFile = await promises.readFile(controlConfPath, {
+    encoding: 'utf-8',
+  });
+  const colorsFile = await promises.readFile(colorConfPath, {
+    encoding: 'utf-8',
+  });
+
+  const controlsRomNames = loadIniFileRomNames(controlsFile);
+  const colorsRomNames = loadIniFileRomNames(colorsFile);
+
+  COINOPS_MAME_ROMS.GEMS = (
+    await promises.readFile(gemsRomsList, {
+      encoding: 'utf-8',
+    })
+  ).split('\n');
+
+  COINOPS_MAME_ROMS.GEMS_PLUS = (
+    await promises.readFile(gemsPlusRomsList, {
+      encoding: 'utf-8',
+    })
+  ).split('\n');
+
+  await promises.writeFile(
+    `./scripts/gemsInfo.json`,
+    JSON.stringify(
+      checkRomsConfig(COINOPS_MAME_ROMS.GEMS, controlsRomNames, colorsRomNames),
+      null,
+      2
+    )
+  );
 
   await promises.writeFile(
     `./scripts/gemsPlusInfo.json`,
-    JSON.stringify(gemsPlusInfos, null, 2)
+    JSON.stringify(
+      checkRomsConfig(
+        COINOPS_MAME_ROMS.GEMS_PLUS,
+        controlsRomNames,
+        colorsRomNames
+      ),
+      null,
+      2
+    )
   );
 
   process.exit(0);
