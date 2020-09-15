73eef7f90d564b25cc62b3bc5048935ed162ddf9
marchah <marchah.dev@gmail.com>
2020-09-14 21:12:18 -0700

    add more launchers WIP

---
+++ plugins/ledblinky-integration/launchers/model2Official.bat
@@ -0,0 +1,28 @@
+@echo off
+call ..\config.cmd
+set rom_name=%1
+
+set "EMULATOR_PATH=..\..\Emulators Official\Sega Model 2"
+
+cd..\..\LEDBlinky
+if %is_ledblinky_activated%==1 (
+  start "" LEDBlinky.exe %rom_name% Sega_Model_2
+)
+
+cd "%EMULATOR_PATH%"
+start /wait /B "" %rom_name%.bat
+
+:WAITLOOP
+tasklist /FI "IMAGENAME eq mame64.exe" 2>NUL | find /I /N "mame64.exe">NUL
+if "%ERRORLEVEL%"=="0" goto RUNNING
+goto NOTRUNNING
+
+:RUNNING
+timeout /t 5
+goto WAITLOOP
+
+:NOTRUNNING
+cd..\..\plugins\LEDBlinky
+if %is_ledblinky_activated%==1 (
+  start "" LEDBlinky.exe %frontend_default_animation%
+)
