bcaff158b47a417cb30d8909f7bf45bd1a21a966
marchah <marchah.dev@gmail.com>
2020-09-18 17:42:03 -0700

    fix(model2/3): add wait command

--- plugins/ledblinky-integration/launchers/model3Official.bat
+++ plugins/ledblinky-integration/launchers/model3Official.bat
@@ -12,6 +12,9 @@ if %is_ledblinky_activated%==1 (
 cd "%EMULATOR_PATH%"
 start /wait /B "" %rom_name%.bat
 
+REM wait for the emulator to have the time to start
+timeout /t 5
+
 :WAITLOOP
 tasklist /FI "IMAGENAME eq %rom_name%.bat" 2>NUL | find /I /N "%rom_name%.bat">NUL
 if "%ERRORLEVEL%"=="0" goto RUNNING
